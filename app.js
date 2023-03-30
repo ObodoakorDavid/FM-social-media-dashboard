13b77d1b9d7cb248221fbbd4f467c68f62225d1e
ObodoakorDavid <obodoakordavid@outlook.com>
2023-03-30 15:50:53 +0100

    final commit, hopefully

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ let slider = document.querySelector(".slider");
 let circle = document.querySelector(".slider span");
 let body = document.querySelector("body");
 
+let darkMode = localStorage.getItem("dark-mode") || null;
+
+if (darkMode) {
+  circle.classList.remove("active");
+  body.classList.remove("light-theme");
+} else {
+  circle.classList.add("active");
+  body.classList.add("light-theme");
+}
+
 slider.addEventListener("click", () => {
-  circle.classList.toggle("active");
-  body.classList.toggle("light-theme");
+  if (body.classList.contains("light-theme")) {
+    circle.classList.remove("active");
+    body.classList.remove("light-theme");
+    localStorage.setItem("dark-mode", true);
+  } else {
+    circle.classList.add("active");
+    body.classList.add("light-theme");
+    localStorage.removeItem("dark-mode");
+  }
 });
